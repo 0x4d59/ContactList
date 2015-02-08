038b56971c54c6e570f77e7cbe9e549c3ea987bc
Yury Molchanov <0x4d59@gmail.com>
2015-02-08 04:45:04 +0300

    repeat and base request

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -1,15 +1,15 @@
-var app = angular.module('ContactList', ['ngRoute']);
+var app = angular.module('ContactList', ['ngRoute', 'firebase']);
 
 app.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
       when('/', {
         templateUrl: '../../views/list.html',
-        controller: 'AppSaverCntrl'
+        controller: 'ListCntrl'
       }).
       when('/tile', {
         templateUrl: '../../views/tile.html',
-        controller: 'AppSaverCntrl'
+        controller: 'ListCntrl'
       }).
       when('/addperson', {
         templateUrl: '../../views/addcontact.html',
@@ -17,7 +17,15 @@ app.config(['$routeProvider',
       })
   }]);
 
-app.controller('AppSaverCntrl', function ($scope) {
+app.controller('AppSaverCntrl', function ($scope, $firebase) {
+  var ref = new Firebase("https://greatcontactlist.firebaseio.com/");
+  var sync = $firebase(ref);
+  var messagesArray = sync.$asArray();
+  console.log(messagesArray);
+  $scope.persons = messagesArray;
+
+
+
   $scope.appStyle = 'list';
   $scope.lists = true;
   $scope.appTitle = 'контакты';
@@ -37,3 +45,12 @@ app.controller('AppSaverCntrl', function ($scope) {
     $scope.appTitle = 'контакты';
   }
 });
+
+app.controller('ListCntrl', function ($scope) {
+  // var ref = new Firebase("https://greatcontactlist.firebaseio.com/");
+  // var sync = $firebase(ref);
+  // var messagesArray = sync.$asArray();
+  // console.log(messagesArray);
+  // $scope.persons = messagesArray;
+
+})
