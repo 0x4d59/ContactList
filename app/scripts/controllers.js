f4a72528b6cfc27282d8ab0112b32dd7a8f5ce9b
Yury Molchanov <0x4d59@gmail.com>
2015-02-08 21:05:40 +0300

    list and tile go to person page

---
+++ app/scripts/controllers.js
@@ -0,0 +1,64 @@
+// controllers
+
+angular.module('ContactList').controller('AppSaverController', ['$scope', '$firebase', function ($scope, $firebase) {
+  
+  var ref = new Firebase("https://greatcontactlist.firebaseio.com/");
+  var sync = $firebase(ref);
+  var messagesArray = sync.$asArray();
+  $scope.persons = messagesArray;
+
+  $scope.appStyle = 'list';
+  $scope.lists = true;
+  $scope.appTitle = 'контакты';
+
+  $scope.changeAppstyle = function (button) {
+    $scope.appStyle = button;
+  };
+
+  $scope.personStyle = function () {
+    $scope.lists = false;
+    $scope.appTitle = 'контакт';
+  };
+
+  $scope.listStyles = function () {
+    $scope.lists = true;
+    $scope.appStyle = 'list';
+    $scope.appTitle = 'контакты';
+  }
+}]);
+
+angular.module('ContactList').controller('AddPersonController', ['$scope', '$firebase', function ($scope, $firebase) {
+  $scope.newPerson = {
+    'name': '',
+    'surname': '',
+    'email': '',
+    'phone': '',
+    'photo': ''
+  };
+  $scope.basePush = function () {
+
+    var ref = new Firebase("https://greatcontactlist.firebaseio.com/");
+    var sync = $firebase(ref);
+
+    var messages = sync.$asArray();
+    messages.$add($scope.newPerson);
+    messages.$save($scope.newPerson)
+  };
+
+  $scope.resetForm = function () {
+    $scope.newPerson = {
+      'name': '',
+      'surname': '',
+      'email': '',
+      'phone': '',
+      'photo': ''
+    };
+  };
+}]);
+
+angular.module('ContactList').controller('ListController', ['$scope', '$location', function ($scope, $location) {
+  $scope.goToPersonPage = function (id) {
+    $scope.personStyle();
+    $location.path('/personpage/' + id);
+  }
+}]);
